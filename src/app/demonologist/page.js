c9dd331a03383d0807062e19e0e450e04df9d793
Tobiasz Szalbot <tobiasz.szalbot@rocksoft.pl>
2023-09-03 19:12:39 +0200

    Fixed page titles

--- src/app/demonologist/page.js
+++ src/app/demonologist/page.js
@@ -2,6 +2,11 @@ import { ghosts } from './ghosts'
 import { evidenceEnum } from './evidence.enum'
 import { Core } from '../components/core/Core'
 
+export const metadata = {
+  title: 'Ghostle - Demonologist',
+  description: 'Guess the ghost from Demonologist!'
+}
+
 export default function Demonologist () {
   return (
     <main className="main">
--- src/app/phasmophobia/page.js
+++ src/app/phasmophobia/page.js
@@ -2,6 +2,11 @@ import { ghosts } from './ghosts'
 import { evidenceEnum } from './evidence.enum'
 import { Core } from '../components/core/Core'
 
+export const metadata = {
+  title: 'Ghostle - Phasmophobia',
+  description: 'Guess the ghost from Phasmophobia!'
+}
+
 export default function Phasmophobia () {
   return (
     <main className="main">
